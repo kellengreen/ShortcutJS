5fc8bc030dc47957cc97ac54f86170cd4cad47bc
Kellen Green <me@kellengreen.com>
2019-02-26 14:30:21 +0000

    Update and rename shortcuts.js to ready.js

---
+++ ready.js
@@ -0,0 +1,9 @@
+export async function ready() {
+    return new Promise(resolve => {
+        if (document.readyState === 'loading') {
+            document.addEventListener('readystatechange', resolve, { once: true })
+        } else {
+            resolve()
+        }
+    })
+}
