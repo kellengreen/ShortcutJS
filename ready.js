f0569e405044782eabae2b419281f1e7f91f3698
Kellen Green <me@kellengreen.com>
2019-08-01 11:16:47 +0100

    Update ready.js

--- ready.js
+++ ready.js
@@ -1,4 +1,7 @@
-export async function ready() {
+/**
+ * Let's us know when the DOM is ready for interaction.
+ */
+export default function ready() {
     return new Promise(resolve => {
         if (document.readyState === 'loading') {
             document.addEventListener('readystatechange', resolve, { once: true })
